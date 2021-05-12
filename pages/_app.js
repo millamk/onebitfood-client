0b59c36effa2dafb282a26ecade67eaaf0ada191
Camilla Kunii <milla.mk@hotmail.com>
2021-05-12 18:48:47 -0300

    Global styles

--- pages/_app.js
+++ pages/_app.js
@@ -4,9 +4,10 @@ import '../styles/globals.scss';
 export default function MyApp({ Component, pageProps }) {
   return(
     <>
-    <Head>
-      <title>OneBitFood V2</title>
-    </Head>
+      <Head>
+        <title>OneBitFood V2</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
     <main>
       <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
+import Container from 'react-bootstrap/Container'; 
+
 export default function Home() {
   return (
     <>
+      <Container> 
+        Home Page Content 
+      </Container> 
     </>
   )
 }
