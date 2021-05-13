a8003c30f7123468ec85bdddab257af5a604961b
Camilla Kunii <milla.mk@hotmail.com>
2021-05-12 23:20:07 -0300

    Connecting Category API

--- services/getRestaurants.js
+++ services/getRestaurants.js
@@ -1,10 +1,18 @@
 import useSWR from 'swr';
+import { useRouter } from 'next/router';
 
 export default function getRestaurants() {
+  const router = useRouter();
+  const { category } = router.query;
+  
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+  let params = '';
+  if(category)
+    params = `${params == '' ? '?' : '&'}category=${category}`
   
   const { data, error } = useSWR(
-    `${process.env.apiUrl}/api/restaurants`,
+    `${process.env.apiUrl}/api/restaurants${params}`,
     fetcher,
     { revalidateOnFocus: false }
   )
